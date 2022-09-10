5ebd49659d801a8f85062e4398aa8fed3c096983
MakhunArtom <super_makhun@ukr.net>
2022-09-10 16:00:10 +0300

    Task-1 compleated

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,36 @@
+const refs = {
+  btnStart: document.querySelector('button[data-start]'),
+  btnStop: document.querySelector('button[data-stop]'),
+  body: document.querySelector('body'),
+};
 
+let timerId = null;
+
+// 1 Зробити динамічний фон боді +
+// 2 Зупиняти іньервал при натисканнні на кнопку старт +
+// 3 Дати умову яка не дозволить натискати на кнопку старт якщо вона активна +
+
+// Старт
+refs.btnStart.addEventListener('click', onBtnStart);
+
+function onBtnStart() {
+  timerId = setInterval(getRandomBodyBgColor, 500);
+
+  refs.btnStart.setAttribute('disabled', 'disabled');
+
+  function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+  function getRandomBodyBgColor() {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }
+}
+
+// Стоп
+refs.btnStop.addEventListener('click', onBtnStop);
+
+function onBtnStop() {
+  clearInterval(timerId);
+  refs.btnStart.removeAttribute('disabled');
+}
---
+++ src/js/02-timer.js
@@ -0,0 +1,6 @@
+setTimeout(() => {
+  console.log('hear - 1');
+}, 0);
+console.log('hear - 2');
+console.log('hear - 3');
+console.log('hear - 4');
